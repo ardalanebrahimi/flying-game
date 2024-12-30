87e9cf847c3d4b875566d058fb13d591c1f90a79
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2024-12-30 21:43:15 +0100

    add start page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './features/start-page/start-page.component';
+import { GameComponent } from './features/game/components/game/game.component';
+import { SkinSelectionPageComponent } from './features/skins/skin-selection-page/skin-selection-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StartPageComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'skin-selection', component: SkinSelectionPageComponent },
+];
