191ce5f1952015b2ef01f88e0a920ad6833aaa42
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2024-12-30 23:00:21 +0100

    add new skins

--- src/app/features/skins/skins.ts
+++ src/app/features/skins/skins.ts
@@ -3,4 +3,7 @@ export const SKINS = [
   { id: 2, name: 'Futuristic Rocket', image: '/skins/rocket1.png' },
   { id: 3, name: 'Tree', image: '/skins/tree2.png' },
   { id: 4, name: 'Lightening', image: '/skins/lightening.png' },
+  { id: 5, name: 'O-Man', image: '/skins/o-man.png' },
+  { id: 6, name: 'Tree-Man', image: '/skins/tree-man.png' },
+  { id: 7, name: 'Red-Man', image: '/skins/red-man.png' },
 ];
